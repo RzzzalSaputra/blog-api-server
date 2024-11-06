8fc761698e1b704a0d4060f21485aaafea990924
Rizal Wahyu Saputra <rizalwahyu819@gmail.com>
2024-11-06 18:38:06 +0700

    "feat: create blog post endpoint validation"

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,11 +1,17 @@
 import express from "express";
+import {body} from "express-validator"
 import { createPost } from "../controllers/post.js";
 import { upload } from "../middlewares/upload.js";
+import { validateRequest } from "../middlewares/valiadteRequest.js";
 
 const router = express.Router()
 
 router.post('/post',
-    upload.single("image"),
+    upload.single("image"),[
+        body('title').not().isEmpty().withMessage('title is required'),
+        body('body').not().isEmpty().withMessage('body is required'),
+    ],
+    validateRequest,
     createPost
 )
 
